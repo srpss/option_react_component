7549b24325d01a67c8622363db611d6b7f2fbc6a
srpss <61586478+srpss@users.noreply.github.com>
2023-03-25 21:05:25 +0200

    css done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 
+import { Select } from "./Select"
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Forth", value: 4 },
+  { label: "Fifth", value: 5 },
+
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-   ""
+    <>
+    <Select options={options}></Select>
+    </>
   )
 }
 
---
+++ vite-project/src/Select.tsx
@@ -0,0 +1,35 @@
+import styles from "./select.module.css"
+
+type SelectOptions = {
+
+    label: string
+    value: any
+}
+
+type SelectProps = {
+    options: SelectOptions[]
+    value?: SelectOptions
+    onChange: (value: SelectOptions | undefined) => void
+
+}
+
+export function Select({ value, onChange, options }: SelectProps) {
+    return (
+
+        <div tabIndex={0} className={styles.container}>
+            <span className={styles.value}>Value</span>
+            <button className={styles["clear-btn"]}>&times;</button>
+            <div className={styles.divider}></div>
+            <div className={styles.caret}></div>
+            <ul className={`${styles.options} ${styles.show}`}>
+                {options.map(option => (
+                    <li key={option.label} className={styles.option}>
+                        {option.label}
+                    </li>
+                ))}
+
+            </ul>
+        </div>
+
+    )
+}
